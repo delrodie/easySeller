8e025642a8ed64714488ab2572b42828c0e4ada4
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-04-26 00:57:59 +0000

    Generation de la classe Approvisionner

---
+++ src/AppBundle/Entity/Approvisionner.php
@@ -0,0 +1,255 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Approvisionner
+ *
+ * @ORM\Table(name="approvisionner")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ApprovisionnerRepository")
+ */
+class Approvisionner
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="reference", type="integer", nullable=true)
+     */
+    private $reference;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="stock", type="array", nullable=true)
+     */
+    private $stock;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Blameable(on="create")
+     * @ORM\Column(name="publie_par", type="string", length=25, nullable=true)
+     */
+    private $publiePar;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Blameable(on="update")
+     * @ORM\Column(name="modifie_par", type="string", length=25, nullable=true)
+     */
+    private $modifiePar;
+
+    /**
+     * @var \DateTime
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="publie_le", type="datetimetz", nullable=true)
+     */
+    private $publieLe;
+
+    /**
+     * @var \DateTime
+     *
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="modifie_le", type="datetimetz", nullable=true)
+     */
+    private $modifieLe;
+
+    /**
+     * @ORM\OneToOne(targetEntity="AppBundle\Entity\Approvisionnement")
+     */
+     private $approvisionnement;
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set reference
+     *
+     * @param integer $reference
+     *
+     * @return Approvisionner
+     */
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    /**
+     * Get reference
+     *
+     * @return int
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+    /**
+     * Set stock
+     *
+     * @param array $stock
+     *
+     * @return Approvisionner
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * Get stock
+     *
+     * @return array
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * Set publiePar
+     *
+     * @param string $publiePar
+     *
+     * @return Approvisionner
+     */
+    public function setPubliePar($publiePar)
+    {
+        $this->publiePar = $publiePar;
+
+        return $this;
+    }
+
+    /**
+     * Get publiePar
+     *
+     * @return string
+     */
+    public function getPubliePar()
+    {
+        return $this->publiePar;
+    }
+
+    /**
+     * Set modifiePar
+     *
+     * @param string $modifiePar
+     *
+     * @return Approvisionner
+     */
+    public function setModifiePar($modifiePar)
+    {
+        $this->modifiePar = $modifiePar;
+
+        return $this;
+    }
+
+    /**
+     * Get modifiePar
+     *
+     * @return string
+     */
+    public function getModifiePar()
+    {
+        return $this->modifiePar;
+    }
+
+    /**
+     * Set publieLe
+     *
+     * @param \DateTime $publieLe
+     *
+     * @return Approvisionner
+     */
+    public function setPublieLe($publieLe)
+    {
+        $this->publieLe = $publieLe;
+
+        return $this;
+    }
+
+    /**
+     * Get publieLe
+     *
+     * @return \DateTime
+     */
+    public function getPublieLe()
+    {
+        return $this->publieLe;
+    }
+
+    /**
+     * Set modifieLe
+     *
+     * @param \DateTime $modifieLe
+     *
+     * @return Approvisionner
+     */
+    public function setModifieLe($modifieLe)
+    {
+        $this->modifieLe = $modifieLe;
+
+        return $this;
+    }
+
+    /**
+     * Get modifieLe
+     *
+     * @return \DateTime
+     */
+    public function getModifieLe()
+    {
+        return $this->modifieLe;
+    }
+
+    /**
+     * Set approvisionnement
+     *
+     * @param \AppBundle\Entity\Approvisionnement $approvisionnement
+     *
+     * @return Approvisionner
+     */
+    public function setApprovisionnement(\AppBundle\Entity\Approvisionnement $approvisionnement = null)
+    {
+        $this->approvisionnement = $approvisionnement;
+
+        return $this;
+    }
+
+    /**
+     * Get approvisionnement
+     *
+     * @return \AppBundle\Entity\Approvisionnement
+     */
+    public function getApprovisionnement()
+    {
+        return $this->approvisionnement;
+    }
+}
