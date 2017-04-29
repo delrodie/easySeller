73073ecc8ec37faf395323d718cec8a6f4f28867
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-04-29 03:29:48 +0000

    Gestion des achats fournisseurs

--- src/AppBundle/Repository/ApprovisionnementRepository.php
+++ src/AppBundle/Repository/ApprovisionnementRepository.php
@@ -44,4 +44,57 @@ class ApprovisionnementRepository extends \Doctrine\ORM\EntityRepository
             return $e;
         }
     }
+
+  /**
+   * Liste des fournisseurs concernés par les approvisionnements
+   *
+   * @author: Delrodie AMOIKON
+   * @date: 28/04/2017 01:37
+   * @version: v1.0
+   */
+   public function getFournisseurByApprovisionnement()
+   {
+     $em = $this->getEntityManager();
+     $qb = $em->createQuery('
+            SELECT DISTINCT(a.fournisseur) as id
+            FROM AppBundle:Approvisionnement a
+     ');
+     try {
+         $result = $qb->getResult();
+
+         return $result;
+
+     } catch (NoResultException $e) {
+         return $e;
+     }
+
+   }
+
+   /**
+    * Statistiques des fournisseurs concernés par les approvisionnements
+    *
+    * @author: Delrodie AMOIKON
+    * @date: 28/04/2017 01:37
+    * @version: v1.0
+    */
+    public function getStatistiquesFournisseur($id)
+    {
+      $em = $this->getEntityManager();
+      $qb = $em->createQuery('
+             SELECT SUM(a.montant) as montant, SUM(a.fret) as fret, SUM(a.valider) as valider
+             FROM AppBundle:Approvisionnement a
+             WHERE a.fournisseur = :id
+      ')->setParameter('id',$id)
+      ;
+      try {
+          $result = $qb->getResult();
+
+          return $result;
+
+      } catch (NoResultException $e) {
+          return $e;
+      }
+
+    }
+
 }
